879587e69d2695e22a53ae7577b84a6968e46f19
VladisNech <VladisZero@yandex.ru>
2019-10-29 00:39:15 +0300

    сделал задачи 2 и 3 в дз по уроку 4

--- test/venv/AllData.py
+++ test/venv/AllData.py
@@ -241,3 +241,49 @@ def user(name, age, city):
 
 print(user(name=input(), age=int(input()), city=input()))
 
+"""""
+Задание №2
+Создайте функцию, принимающую на вход 3 числа и возвращающую наибольшее из них.
+"""""
+
+
+def user(a, b, c):
+    result = max(a, b, c)
+    return result
+
+
+print(user(a=int(input()), b=int(input()), c=int(input())))
+
+"""""
+Задание №3
+Давайте опишем пару сущностей player и enemy через словарь, который будет иметь ключи и значения:
+name - строка полученная от пользователя,
+health = 100,
+damage = 50. 
+### Поэкспериментируйте со значениями урона и жизней по желанию. 
+### Теперь надо создать функцию attack(person1, person2). Примечание: имена аргументов можете указать свои. 
+### Функция в качестве аргумента будет принимать атакующего и атакуемого. 
+### В теле функция должна получить параметр damage атакующего и отнять это количество от health атакуемого. 
+Функция должна сама работать со словарями и изменять их значения.
+"""""
+
+player = {'name': input(),
+          'health': 100,
+          'damage': 50}
+
+enemy = {'name': input(),
+         'health': 200,
+         'damage': 40}
+
+
+def attack(person1):
+    if person1 == enemy['name']:
+        player['health'] = player['health'] - enemy['damage']
+        return player['health']
+    elif person1 == player['name']:
+        enemy['health'] = enemy['health'] - player['damage']
+        return enemy['health']
+
+
+print(attack(person1=input()))
+
--- test/venv/GeekBrains.py
+++ test/venv/GeekBrains.py
@@ -1,15 +1,33 @@
 # Урок 4
 """""
-Задание №1
-Создайте функцию, принимающую на вход имя, возраст и город проживания человека.
-Функция должна возвращать строку вида «Василий, 21 год(а), проживает в городе Москва»
+Задание №3
+Давайте опишем пару сущностей player и enemy через словарь, который будет иметь ключи и значения:
+name - строка полученная от пользователя,
+health = 100,
+damage = 50. 
+### Поэкспериментируйте со значениями урона и жизней по желанию. 
+### Теперь надо создать функцию attack(person1, person2). Примечание: имена аргументов можете указать свои. 
+### Функция в качестве аргумента будет принимать атакующего и атакуемого. 
+### В теле функция должна получить параметр damage атакующего и отнять это количество от health атакуемого. 
+Функция должна сама работать со словарями и изменять их значения.
 """""
 
+player = {'name': input(),
+          'health': 100,
+          'damage': 50}
 
-def user(name, age, city):
-    result = f'{name}, {age} год(а), проживает в городе {city}'
-    return result
+enemy = {'name': input(),
+         'health': 200,
+         'damage': 40}
 
 
-print(user(name=input(), age=int(input()), city=input()))
+def attack(person1):
+    if person1 == enemy['name']:
+        player['health'] = player['health'] - enemy['damage']
+        return player['health']
+    elif person1 == player['name']:
+        enemy['health'] = enemy['health'] - player['damage']
+        return enemy['health']
 
+
+print(attack(person1=input()))
