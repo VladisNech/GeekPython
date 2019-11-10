228658990ebc164e7e09dc7061cc13d7e15c5717
VladisNech <VladisZero@yandex.ru>
2019-11-10 22:56:54 +0300

    работа с терминальной строкой

--- GeekPython/DraftList.py
+++ GeekPython/DraftList.py
@@ -195,5 +195,15 @@ print(math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2))
 
 print(math.factorial(9))
 # -------------------------------------------------------------------------------------------------------------------
+# Создание папок
+
+import os
+import sys
+
+name = sys.platform
+
+for i in range(1, 6):
+    new_path = os.path.join(os.getcwd(), '{}_{}'.format(name, i))
+    os.mkdir(new_path)
 
 
