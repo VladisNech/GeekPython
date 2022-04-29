33d5631ef2ee993a99bf7bd2443b16c0f48e3ef7
Vladis <vladiszero@yandex.ru>
2022-04-29 04:09:03 +0300

    Дополнил генерацией списков

--- ML/Generation before the first success.py
+++ ML/Generation before the first success.py
@@ -1,10 +1,10 @@
-import random
+from random import uniform
 
-X = random.uniform(0, 1)
+X = uniform(0, 1)
 print("X / 2 =", X / 2)
 n = 0
 while True:
-    Y = random.uniform(0, 1)
+    Y = uniform(0, 1)
     print(f"Y{n + 1}", "=", Y)
     n += 1
     if Y <= X / 2:
